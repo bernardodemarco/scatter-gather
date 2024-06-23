2a167e8e5f6f1fd8b9c7200ea76126c22f6d5a77
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2024-06-23 19:46:25 -0300

    clean client connection

--- src/main/java/com/github/bernardodemarco/textretrieval/communication/client/TCPClientConnection.java
+++ src/main/java/com/github/bernardodemarco/textretrieval/communication/client/TCPClientConnection.java
@@ -1,7 +1,7 @@
 package com.github.bernardodemarco.textretrieval.communication.client;
 
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 import java.io.PrintWriter;
 import java.io.IOException;
