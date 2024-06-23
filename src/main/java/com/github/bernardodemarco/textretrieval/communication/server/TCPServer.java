ffb1f5d05f29f5eb170d0466f164c2cc5321a992
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2024-06-23 19:47:29 -0300

    clean server

--- src/main/java/com/github/bernardodemarco/textretrieval/communication/server/TCPServer.java
+++ src/main/java/com/github/bernardodemarco/textretrieval/communication/server/TCPServer.java
@@ -1,14 +1,15 @@
 package com.github.bernardodemarco.textretrieval.communication.server;
 
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import java.net.Socket;
+import java.net.ServerSocket;
 
 public class TCPServer implements Server {
     private final Logger logger = LogManager.getLogger(getClass());
