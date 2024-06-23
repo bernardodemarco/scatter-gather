909463680c9ff971b84d3fe92add3415104e9eb7
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2024-06-23 15:44:22 -0300

    add server interface

--- src/main/java/com/github/bernardodemarco/textretrieval/communication/server/Server.java
+++ src/main/java/com/github/bernardodemarco/textretrieval/communication/server/Server.java
@@ -1,59 +1,8 @@
-package com.github.bernardodemarco.textretrieval.communication;
+package com.github.bernardodemarco.textretrieval.communication.server;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-public class Server {
-    private final Logger logger = LogManager.getLogger(getClass());
-
-    private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
-
-    public void listen(int port) {
-        try {
-            serverSocket = new ServerSocket(port);
-            clientSocket = serverSocket.accept();
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            logger.info("Listening on port {}.", port);
-        } catch (IOException e) {
-            logger.error("Exception while listening on port {} or listening for a connection.", port, e);
-            System.exit(1);
-        }
-    }
-
-    public void stop() {
-        try {
-            in.close();
-            out.close();
-            clientSocket.close();
-            serverSocket.close();
-        } catch (IOException e) {
-            logger.error("Exception while closing the sockets and their streams.", e);
-            System.exit(1);
-        }
-    }
-
-    public void send(String message) {
-        if (out != null) {
-            out.println(message);
-        }
-    }
-
-    public String receive() {
-        try {
-            return (in != null) ? in.readLine() : null;
-        } catch (IOException e) {
-            return null;
-        }
-    }
+public interface Server {
+    void listen(int port);
+    void stop();
+    void send(String message);
+    String receive();
 }
---
+++ src/main/java/com/github/bernardodemarco/textretrieval/communication/server/TCPServer.java
@@ -0,0 +1,63 @@
+package com.github.bernardodemarco.textretrieval.communication.server;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer implements Server {
+    private final Logger logger = LogManager.getLogger(getClass());
+
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    @Override
+    public void listen(int port) {
+        try {
+            serverSocket = new ServerSocket(port);
+            clientSocket = serverSocket.accept();
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            logger.info("Listening on port {}.", port);
+        } catch (IOException e) {
+            logger.error("Exception while listening on port {} or listening for a connection.", port, e);
+            System.exit(1);
+        }
+    }
+
+    @Override
+    public void stop() {
+        try {
+            in.close();
+            out.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            logger.error("Exception while closing the sockets and their streams.", e);
+            System.exit(1);
+        }
+    }
+
+    @Override
+    public void send(String message) {
+        if (out != null) {
+            out.println(message);
+        }
+    }
+
+    @Override
+    public String receive() {
+        try {
+            return (in != null) ? in.readLine() : null;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+}
